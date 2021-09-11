459d6d9ca32d633960f7bde59ce9e8c6ce012e76
tinyelf <58679441+tinyelf@users.noreply.github.com>
2021-09-10 21:32:39 -0700

    changes for desktop view

--- answer.js
+++ answer.js
@@ -1,10 +1,13 @@
 // When the user clicks on the button,toggle between hiding and showing the dropdown content//
  var acc = document.getElementsByClassName("question");
- var i;
 
- for (i = 0; i < acc.length; i++) {
+ for (let i = 0; i < acc.length; i++) {
    acc[i].addEventListener("click", function() {
      this.classList.toggle("active");
+     for (j = 0; j < acc.length; j++) {
+       if (j !== i)
+        acc[j].nextElementSibling.style.display = "none";
+     }
      var answer = this.nextElementSibling;
      if (answer.style.display === "block") {
        answer.style.display = "none";
